c4225293d5a3a3bba95374004e5e3c07c95d43fb
moshi <sy.41211@gmail.com>
2023-03-03 23:18:37 +0900

    Change v0.1.0 to v0.1.1

--- src/cafeplotter/__init__.py
+++ src/cafeplotter/__init__.py
@@ -3,7 +3,7 @@ import matplotlib as mpl
 from cafeplotter.cafeparser import CafeParser
 from cafeplotter.treeplotter import TreePlotter
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 __all__ = [
     "CafeParser",
